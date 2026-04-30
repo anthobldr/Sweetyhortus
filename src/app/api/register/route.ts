e3ac37f9046a03f68b7cd84490118e42da8ff587
anthobldr <anthony.lutard33@gmail.com>
2026-04-30 22:18:45 +0200

    fix(sql): Modifying the SQL query using a transaction

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,25 +1,42 @@
 import { db } from "@/db/db";
-import { users } from "@/db/schema";
+import { users, users_account } from "@/db/schema";
 import { NextResponse } from "next/server";
 import * as argon2 from 'argon2';
 import { eq } from "drizzle-orm";
 import { z } from "zod"
 
 const registerSchema = z.object({
+    surname: z.string(),
+    name: z.string(),
     email: z.string().email(),
     password: z.string().min(6),
 });
 
 export async function POST(req: Request){
     const body = await req.json();
-    const { email, password } = registerSchema.parse(body)
+    const { surname, name, email, password } = registerSchema.parse(body)
 
     const existing = await db.query.users.findFirst({where: eq(users.email, email)})
     if(existing){
         return NextResponse.json({error: "L'utilisateur existe déjà."}, {status: 400})
     }
     const hashPassword = await argon2.hash(password);
-    const user = await db.insert(users).values({email, password: hashPassword});
+    // Création d'un utilisateur
+    //const user = await db.insert(users).values({email, password: hashPassword}).returning({id: users.id});
+    //const userId = user[0].id;
+    // Création d'un profil à l'utilisateur
+    //const userProfil = await db.insert(users_account).values({userId, name, surname});
+    await db.transaction(async (transaction) => {
+    const [{ id: userId }] = await transaction
+        .insert(users)
+        .values({ email, password: hashPassword })
+        .returning({ id: users.id });
 
-    return NextResponse.json({success: true, user})
+        await transaction.insert(users_account).values({
+            users_id: userId,
+            surname: surname,
+            name: name,
+        });
+    });
+    return NextResponse.json({success: true, users})
 }
